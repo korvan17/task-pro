d58a2c65de24d91ff6b83afcfb6be93c1ad00498
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-08-24 13:30:24 +0300

    test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,19 @@
 export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      Welcome to TASK PRO
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        Welcome to TASK PRO
+      </div>
+      <p>TEST</p>
+    </>
   );
 }
