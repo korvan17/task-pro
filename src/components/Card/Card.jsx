6454be21b25344d104e488ec8bcdd22d76a100b1
savelii <savvageljinskiy@gmail.com>
2023-08-24 19:02:47 -0400

    feat: add basic markup & styling of card
    Added Card component and its styles, created function for detection the color by priority
    
    Task: https://trello.com/c/2yjZghf0

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,42 @@
-export default function Task() {
-  return <h2>this is a task</h2>;
+import React from 'react';
+import css from './Card.module.css';
+
+export default function Card({ title, desc, priority, deadline }) {
+  const getColor = () => {
+    switch (priority) {
+      case 'high':
+        return '#bedbb0';
+      case 'medium':
+        return '#e09cb5';
+      case 'low':
+        return '#8fa1d0';
+      default:
+        return '#bababa';
+    }
+  };
+
+  const priorityStyle = {
+    '--priority-color': getColor(),
+  };
+
+  return (
+    <div className={css.overWrapper}>
+      <div className={css.wrapper} style={priorityStyle}>
+        <h4 className={css.title}>{title}</h4>
+        <p className={css.desc}>{desc}</p>
+        <div className={css.additionWrapper}>
+          <div className={css.additionInfo}>
+            <p className={css.additionInfoHeading}>priority</p>
+            <p className={css.priority} style={priorityStyle}>
+              {priority}
+            </p>
+          </div>
+          <div className={css.additionInfo}>
+            <p className={css.additionInfoHeading}>deadline</p>
+            <p>{deadline}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
