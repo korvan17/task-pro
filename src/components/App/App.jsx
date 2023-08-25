72b79627d219cf460b2c689623f4f52f33ef46f5
Ivan Kornienko <ivan@homeds.pro>
2023-08-25 16:21:30 +0300

    intex to path

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route index element={<StartPage />} />
+        <Route path="/" element={<StartPage />} />
         <Route path="/home" element={<Main />} />
         <Route
           path="/privathomepage"
