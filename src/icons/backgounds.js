3a2671681a51ada2f6bdb14cabe569e649a9189d
Ivan Kornienko <ivan@homeds.pro>
2023-08-26 19:59:01 +0300

    bachground

---
+++ src/icons/backgounds.js
@@ -0,0 +1,108 @@
+const tree = {
+  tablet2x:
+    'https://res.cloudinary.com/task-pro/image/upload/v1692957366/backgrounds/tree-tablet-2x.jpg',
+  tablet:
+    'https://res.cloudinary.com/task-pro/image/upload/v1692957366/backgrounds/tree-tablet.jpg',
+  mob2x:
+    'https://res.cloudinary.com/task-pro/image/upload/v1692957366/backgrounds/tree-mob-2x.jpg',
+  // https://res.cloudinary.com/task-pro/image/upload/v1692957366/backgrounds/tree-mob.jpg
+  // https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/tree-desktop-2x.jpg
+  // https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/tree-desktop.jpg
+};
+// https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/trailer-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/trailer-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/trailer-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/trailer-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/trailer-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957363/backgrounds/trailer-desktop.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-desktop.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-desktop-2x.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/semimoon-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/semimoon-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/semimoon-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/semimoon-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/semimoon-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/semimoon-desktop.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/sea-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/sea-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/sea-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/sea-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/sea-desktop.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/sea-mob.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-desktop.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/mountains-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/mountains-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/mountains-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957358/backgrounds/mountains-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957357/backgrounds/mountains-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957357/backgrounds/mountains-desktop.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957357/backgrounds/leaves-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/leaves-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/leaves-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/leaves-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/leaves-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/leaves-desktop.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/fullmoon-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/fullmoon-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/fullmoon-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/fullmoon-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/fullmoon-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/fullmoon-desktop.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-desktop.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/clouds-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/clouds-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/clouds-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/clouds-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/clouds-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/clouds-desktop.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/cave-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/cave-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/cave-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/cave-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/cave-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/cave-desktop.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-desktop.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/balloonFar-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/balloonFar-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonFar-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonFar-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonFar-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonFar-desktop.jpg
+
+// https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonClose-desktop-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonClose-tablet-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonClose-mob-2x.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonClose-mob.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957351/backgrounds/balloonClose-tablet.jpg
+// https://res.cloudinary.com/task-pro/image/upload/v1692957351/backgrounds/balloonClose-desktop.jpg
