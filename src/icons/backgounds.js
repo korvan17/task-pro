4caf87624306bc69e8e9fae433d14fec98db0a9b
Ivan Kornienko <ivan@homeds.pro>
2023-08-26 22:34:08 +0300

    add baseurl in background

--- src/icons/backgounds.js
+++ src/icons/backgounds.js
@@ -1,209 +1,136 @@
+const BASE_URL = `https://res.cloudinary.com/task-pro/image/upload/v16929573`;
+// const SECOND_URL = '/backgrounds/';
 export const tree = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957366/backgrounds/tree-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957366/backgrounds/tree-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957366/backgrounds/tree-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957366/backgrounds/tree-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/tree-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/tree-desktop.jpg',
+  tablet2x: `${BASE_URL}66/backgrounds/tree-tablet-2x.jpg`,
+  tablet: `${BASE_URL}66/backgrounds/tree-tablet.jpg`,
+  mob2x: `${BASE_URL}66/backgrounds/tree-mob-2x.jpg`,
+  mob: `${BASE_URL}66/backgrounds/tree-mob.jpg`,
+  desktop2x: `${BASE_URL}65/backgrounds/tree-desktop-2x.jpg`,
+  desktop: `${BASE_URL}65/backgrounds/tree-desktop.jpg`,
 };
 
 export const triler = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/trailer-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/trailer-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/trailer-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/trailer-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957365/backgrounds/trailer-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957363/backgrounds/trailer-desktop.jpg',
+  tablet2x: `${BASE_URL}65/backgrounds/trailer-tablet-2x.jpg`,
+  tablet: `${BASE_URL}65/backgrounds/trailer-tablet.jpg`,
+  mob2x: `${BASE_URL}65/backgrounds/trailer-mob-2x.jpg`,
+  mob: `${BASE_URL}65/backgrounds/trailer-mob.jpg`,
+  desktop2x: `${BASE_URL}65/backgrounds/trailer-desktop-2x.jpg`,
+  desktop: `${BASE_URL}63/backgrounds/trailer-desktop.jpg`,
 };
 
 export const spheres = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/spheres-desktop.jpg',
+  tablet2x: `${BASE_URL}62/backgrounds/spheres-tablet-2x.jpg`,
+  tablet: `${BASE_URL}62/backgrounds/spheres-tablet.jpg`,
+  mob2x: `${BASE_URL}62/backgrounds/spheres-mob-2x.jpg`,
+  mob: `${BASE_URL}62/backgrounds/spheres-mob.jpg`,
+  desktop2x: `${BASE_URL}62/backgrounds/spheres-desktop-2x.jpg`,
+  desktop: `${BASE_URL}62/backgrounds/spheres-desktop.jpg`,
 };
 
 export const semimoon = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957362/backgrounds/semimoon-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/semimoon-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/semimoon-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/semimoon-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/semimoon-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/semimoon-desktop.jpg',
+  tablet2x: `${BASE_URL}62/backgrounds/semimoon-tablet-2x.jpg`,
+  tablet: `${BASE_URL}61/backgrounds/semimoon-tablet.jpg`,
+  mob2x: `${BASE_URL}61/backgrounds/semimoon-mob-2x.jpg`,
+  mob: `${BASE_URL}61/backgrounds/semimoon-mob.jpg`,
+  desktop2x: `${BASE_URL}61/backgrounds/semimoon-desktop-2x.jpg`,
+  desktop: `${BASE_URL}61/backgrounds/semimoon-desktop.jpg`,
 };
 
 export const sea = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/sea-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/sea-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/sea-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/sea-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957361/backgrounds/sea-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/sea-desktop.jpg',
+  tablet2x: `${BASE_URL}61/backgrounds/sea-tablet-2x.jpg`,
+  tablet: `${BASE_URL}61/backgrounds/sea-tablet.jpg`,
+  mob2x: `${BASE_URL}61/backgrounds/sea-mob-2x.jpg`,
+  mob: `${BASE_URL}60/backgrounds/sea-mob.jpg`,
+  desktop2x: `${BASE_URL}61/backgrounds/sea-desktop-2x.jpg`,
+  desktop: `${BASE_URL}60/backgrounds/sea-desktop.jpg`,
 };
 
 export const rocks = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/rocks-desktop.jpg',
+  tablet2x: `${BASE_URL}60/backgrounds/rocks-tablet-2x.jpg`,
+  tablet: `${BASE_URL}60/backgrounds/rocks-tablet.jpg`,
+  mob2x: `${BASE_URL}60/backgrounds/rocks-mob-2x.jpg`,
+  mob: `${BASE_URL}60/backgrounds/rocks-mob.jpg`,
+  desktop2x: `${BASE_URL}60/backgrounds/rocks-desktop-2x.jpg`,
+  desktop: `${BASE_URL}60/backgrounds/rocks-desktop.jpg`,
 };
 
 export const mountains = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/mountains-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/mountains-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957360/backgrounds/mountains-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957358/backgrounds/mountains-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957357/backgrounds/mountains-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957357/backgrounds/mountains-desktop.jpg ',
+  tablet2x: `${BASE_URL}60/backgrounds/mountains-tablet-2x.jpg`,
+  tablet: `${BASE_URL}60/backgrounds/mountains-tablet.jpg`,
+  mob2x: `${BASE_URL}60/backgrounds/mountains-mob-2x.jpg`,
+  mob: `${BASE_URL}58/backgrounds/mountains-mob.jpg`,
+  desktop2x: `${BASE_URL}57/backgrounds/mountains-desktop-2x.jpg`,
+  desktop: `${BASE_URL}57/backgrounds/mountains-desktop.jpg `,
 };
 
 export const leaves = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957357/backgrounds/leaves-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/leaves-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/leaves-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/leaves-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/leaves-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/leaves-desktop.jpg',
+  tablet2x: `${BASE_URL}57/backgrounds/leaves-tablet-2x.jpg`,
+  tablet: `${BASE_URL}56/backgrounds/leaves-tablet.jpg`,
+  mob2x: `${BASE_URL}56/backgrounds/leaves-mob-2x.jpg`,
+  mob: `${BASE_URL}56/backgrounds/leaves-mob.jpg`,
+  desktop2x: `${BASE_URL}56/backgrounds/leaves-desktop-2x.jpg`,
+  desktop: `${BASE_URL}56/backgrounds/leaves-desktop.jpg`,
 };
 
 export const fullmoon = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/fullmoon-tablet.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/fullmoon-tablet-2x.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/fullmoon-mob.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957356/backgrounds/fullmoon-mob-2x.jpg,',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/fullmoon-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/fullmoon-desktop.jpg',
+  tablet2x: `${BASE_URL}56/backgrounds/fullmoon-tablet.jpg`,
+  tablet: `${BASE_URL}56/backgrounds/fullmoon-tablet-2x.jpg`,
+  mob2x: `${BASE_URL}56/backgrounds/fullmoon-mob.jpg`,
+  mob: `${BASE_URL}56/backgrounds/fullmoon-mob-2x.jpg,`,
+  desktop2x: `${BASE_URL}55/backgrounds/fullmoon-desktop-2x.jpg`,
+  desktop: `${BASE_URL}55/backgrounds/fullmoon-desktop.jpg`,
 };
 
 export const flowers = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/flowers-desktop.jpg',
+  tablet2x: `${BASE_URL}55/backgrounds/flowers-tablet-2x.jpg`,
+  tablet: `${BASE_URL}55/backgrounds/flowers-tablet.jpg`,
+  mob2x: `${BASE_URL}55/backgrounds/flowers-mob-2x.jpg`,
+  mob: `${BASE_URL}55/backgrounds/flowers-mob.jpg`,
+  desktop2x: `${BASE_URL}55/backgrounds/flowers-desktop-2x.jpg`,
+  desktop: `${BASE_URL}55/backgrounds/flowers-desktop.jpg`,
 };
 
 export const clouds = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/clouds-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957355/backgrounds/clouds-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/clouds-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/clouds-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/clouds-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/clouds-desktop.jpg',
+  tablet2x: `${BASE_URL}55/backgrounds/clouds-tablet-2x.jpg`,
+  tablet: `${BASE_URL}55/backgrounds/clouds-tablet.jpg`,
+  mob2x: `${BASE_URL}54/backgrounds/clouds-mob-2x.jpg`,
+  mob: `${BASE_URL}54/backgrounds/clouds-mob.jpg`,
+  desktop2x: `${BASE_URL}54/backgrounds/clouds-desktop-2x.jpg`,
+  desktop: `${BASE_URL}54/backgrounds/clouds-desktop.jpg`,
 };
 
 export const cave = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/cave-tablet.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/cave-tablet-2x.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/cave-mob.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/cave-mob-2x.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957354/backgrounds/cave-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/cave-desktop.jpg',
+  tablet2x: `${BASE_URL}54/backgrounds/cave-tablet.jpg`,
+  tablet: `${BASE_URL}54/backgrounds/cave-tablet-2x.jpg`,
+  mob2x: `${BASE_URL}54/backgrounds/cave-mob.jpg`,
+  mob: `${BASE_URL}54/backgrounds/cave-mob-2x.jpg`,
+  desktop2x: `${BASE_URL}54/backgrounds/cave-desktop-2x.jpg`,
+  desktop: `${BASE_URL}53/backgrounds/cave-desktop.jpg`,
 };
 
 export const boat = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/boat-desktop.jpg',
+  tablet2x: `${BASE_URL}53/backgrounds/boat-tablet-2x.jpg`,
+  tablet: `${BASE_URL}53/backgrounds/boat-tablet.jpg`,
+  mob2x: `${BASE_URL}53/backgrounds/boat-mob-2x.jpg`,
+  mob: `${BASE_URL}53/backgrounds/boat-mob.jpg`,
+  desktop2x: `${BASE_URL}53/backgrounds/boat-desktop-2x.jpg`,
+  desktop: `${BASE_URL}53/backgrounds/boat-desktop.jpg`,
 };
 
 export const balloonFar = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/balloonFar-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonFar-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957353/backgrounds/balloonFar-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonFar-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonFar-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonFar-desktop.jpg',
+  tablet2x: `${BASE_URL}53/backgrounds/balloonFar-tablet-2x.jpg`,
+  tablet: `${BASE_URL}52/backgrounds/balloonFar-tablet.jpg`,
+  mob2x: `${BASE_URL}53/backgrounds/balloonFar-mob-2x.jpg`,
+  mob: `${BASE_URL}52/backgrounds/balloonFar-mob.jpg`,
+  desktop2x: `${BASE_URL}52/backgrounds/balloonFar-desktop-2x.jpg`,
+  desktop: `${BASE_URL}52/backgrounds/balloonFar-desktop.jpg`,
 };
 
 export const balloonClose = {
-  tablet2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonClose-tablet-2x.jpg',
-  tablet:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957351/backgrounds/balloonClose-tablet.jpg',
-  mob2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonClose-mob-2x.jpg',
-  mob: 'https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonClose-mob.jpg',
-  desktop2x:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957352/backgrounds/balloonClose-desktop-2x.jpg',
-  desktop:
-    'https://res.cloudinary.com/task-pro/image/upload/v1692957351/backgrounds/balloonClose-desktop.jpg',
+  tablet2x: `${BASE_URL}52/backgrounds/balloonClose-tablet-2x.jpg`,
+  tablet: `${BASE_URL}51/backgrounds/balloonClose-tablet.jpg`,
+  mob2x: `${BASE_URL}52/backgrounds/balloonClose-mob-2x.jpg`,
+  mob: `${BASE_URL}52/backgrounds/balloonClose-mob.jpg`,
+  desktop2x: `${BASE_URL}52/backgrounds/balloonClose-desktop-2x.jpg`,
+  desktop: `${BASE_URL}51/backgrounds/balloonClose-desktop.jpg`,
 };
