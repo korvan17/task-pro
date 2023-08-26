822044d615356750b22554ab9ca6ff7f84282769
Yurii Soroka <yurisoroka165@gmail.com>
2023-08-26 14:48:43 +0300

    up

--- src/components/Controllers/Theme/Theme.jsx
+++ src/components/Controllers/Theme/Theme.jsx
@@ -1,3 +1,38 @@
+import { useState } from 'react';
+import iconDefs from '../../../icons/sprite.svg';
+import css from './Theme.module.css';
+
 export default function Theme() {
-  return <h3>theme</h3>;
+  const themes = ['light', 'dark', 'violet'];
+  const [isThemeListVisible, setIsThemeListVisible] = useState(false);
+
+  const toggleThemeList = () => {
+    setIsThemeListVisible(!isThemeListVisible);
+  };
+
+  return (
+    <div className={css.themeContainer}>
+      <button className={css.themeChangerButton} onClick={toggleThemeList}>
+        <h3>Theme</h3>
+        <svg className={css.themeChangerIcon} width="16" height="16">
+          <use xlinkHref={`${iconDefs}#icon-theme`} />
+        </svg>
+      </button>
+      <div
+        className={`${css.themeListContainer}${
+          isThemeListVisible ? '.active' : ''
+        }`}
+      >
+        <ul className={css.themeList}>
+          {themes.map(theme => (
+            <li key={theme + 1} className={css.themeListButton}>
+              <button className={css.themeListButton} onClick={toggleThemeList}>
+                {theme[0].toUpperCase() + theme.slice(1)}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
