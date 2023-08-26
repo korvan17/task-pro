08fa53eec7b5d980bdd8426fda51a48883225587
bakmaiev <thisyoungrhyme@gmail.com>
2023-08-26 19:38:53 +0300

    Add new changes

--- src/components/Modals/AddEditColumn/AddEditColumn.jsx
+++ src/components/Modals/AddEditColumn/AddEditColumn.jsx
@@ -1,3 +1,28 @@
-export default function AddEditColumn() {
-  return <h3>add or edit card</h3>;
+import css from './AddEditColumn.module.css';
+import iconDefs from '../../../icons/sprite.svg';
+
+export default function AddEditColumn({ title, onClose, isEditing = false }) {
+  return (
+    <>
+      <button className={css.closeBtn}>
+        <svg width="18" height="18">
+          <use xlinkHref={`${iconDefs}#icon-close`} />
+        </svg>
+      </button>
+      <h3 className={css.titleBoard}>
+        {!isEditing ? 'Add column' : 'Edit column'}
+      </h3>
+      <form>
+        <label className={css.label}>
+          <input
+            className={css.input}
+            type="text"
+            name="title"
+            placeholder="Title"
+          />
+        </label>
+        <button className={css.btn}>Add</button>
+      </form>
+    </>
+  );
 }
