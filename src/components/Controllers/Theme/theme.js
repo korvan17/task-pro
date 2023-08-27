3a6ee687d2435a49adba1c1a81eeea7adff2ce32
Yurii Soroka <yurisoroka165@gmail.com>
2023-08-27 13:28:20 +0300

    up

--- src/components/Controllers/Theme/theme.js
+++ src/components/Controllers/Theme/theme.js
@@ -1,6 +1,35 @@
 export const light = {
-  primaryBackground: '#F6F6F7',
-  sideBarBackground: '#FFFFFF',
+  sidebar: {
+    background: '#FFFFFF',
+    logoFill: '#1F1F1F',
+    logoFlashColor: '#FFFFFF',
+    logoTextColor: '#161616',
+    myBoardsColor: 'rgba(22, 22, 22, 0.5)',
+    separatorLIneColor: 'rgba(22, 22, 22, 0.1)',
+    createBoardColor: '#161616',
+    createButtonBAckground: '#BEDBB0',
+    createButtonPlusFill: '#121212',
+    selectedBoardBackground: '#F6F6F7',
+    selectedBoardRightBorder: '#BEDBB0',
+    selectedBoardIconFill: '#161616',
+    selectedBoardTitleColor: '#161616',
+    selectedBoardPencilFill: 'rgba(22, 22, 22, 0.5)',
+    selectedBoardTrashFill: 'rgba(22, 22, 22, 0.5)',
+    boardIconFill: 'rgba(22, 22, 22, 0.5)',
+    boardTitleColor: 'rgba(22, 22, 22, 0.5)',
+    needHelpBackground: '#F6F6F7',
+    needHelpTextColor: '#161616',
+    needHelpSpanColor: '#BEDBB0',
+    needHelpIconAndTextColor: '#161616',
+    logoutIconFill: '#BEDBB0',
+    logoutTextColor: '#161616',
+  },
+  header: {
+    background: '#FCFCFC',
+  },
+  screensPage: {},
+  // primaryBackground: '#F6F6F7',
+  // sideBarBackground: '#FFFFFF',
 };
 
 export const dark = {
