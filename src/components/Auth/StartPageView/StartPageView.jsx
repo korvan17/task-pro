f4868baba1f69a0d9c4770ef65d33c4de484bb03
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-08-27 21:42:39 +0300

    fix styles

--- src/components/Auth/StartPageView/StartPageView.jsx
+++ src/components/Auth/StartPageView/StartPageView.jsx
@@ -2,37 +2,10 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import css from './StartPageView.module.css';
 import svgSprite from '../../../icons/sprite.svg';
-// import img from '../../../images/mob-boy-img-1x.png';
 
 function StartPageView() {
   return (
     <div className={css.startPageSection}>
-      {/* <img src={img} className={css.startPageImage} alt="user"></img> */}
-      {/* <picture>
-        <source
-          srcSet="
-                    ../../../images/mob-boy-img-1x.png 1x,
-                    ../../../images/mob-boy-img-2x.png 2x
-                  "
-          media="(max-width: 767px)"
-        />
-        <source
-          srcSet="
-                    ../../../images/tablet-boy-img-1x.png 1x,
-                    ../../../images/tablet-boy-img-2x.png 2x
-                  "
-          media="(max-width: 1199px)"
-        />
-        <source
-          srcSet="
-                    ../../../images/desk-boy-img-1x.png 1x,
-                    ../../../images/desk-boy-img-1x.png 2x
-                  "
-          media="(min-width: 1200px)"
-        />
-        <img src="../../../images/mob-boy-img-1x.png" alt="user" height="260" />
-      </picture> */}
-
       <picture className={css.startPageImage}>
         <source
           srcSet={
