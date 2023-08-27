806c77067682225ae068bff809e69bd2bfbfdacc
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-08-27 21:28:26 +0300

    add boy image

--- src/components/Auth/StartPageView/StartPageView.jsx
+++ src/components/Auth/StartPageView/StartPageView.jsx
@@ -1,11 +1,72 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import css from './StartPageView.module.css';
+// import img from '../../../images/mob-boy-img-1x.png';
 
 function StartPageView() {
   return (
     <div className={css.startPageSection}>
-      <p className={css.startPageImage}>Boy image</p>
+      {/* <img src={img} className={css.startPageImage} alt="user"></img> */}
+      {/* <picture>
+        <source
+          srcSet="
+                    ../../../images/mob-boy-img-1x.png 1x,
+                    ../../../images/mob-boy-img-2x.png 2x
+                  "
+          media="(max-width: 767px)"
+        />
+        <source
+          srcSet="
+                    ../../../images/tablet-boy-img-1x.png 1x,
+                    ../../../images/tablet-boy-img-2x.png 2x
+                  "
+          media="(max-width: 1199px)"
+        />
+        <source
+          srcSet="
+                    ../../../images/desk-boy-img-1x.png 1x,
+                    ../../../images/desk-boy-img-1x.png 2x
+                  "
+          media="(min-width: 1200px)"
+        />
+        <img src="../../../images/mob-boy-img-1x.png" alt="user" height="260" />
+      </picture> */}
+
+      <picture className={css.startPageImage}>
+        <source
+          srcSet={
+            require('../../../images/mob-boy-img-1x.png') +
+            ' 1x,' +
+            require('../../../images/mob-boy-img-2x.png') +
+            ' 2x'
+          }
+          media="(max-width: 767px)"
+        />
+        <source
+          srcSet={
+            require('../../../images/tablet-boy-img-1x.png') +
+            ' 1x,' +
+            require('../../../images/tablet-boy-img-2x.png') +
+            ' 2x'
+          }
+          media="(max-width: 1199px)"
+        />
+        <source
+          srcSet={
+            require('../../../images/desk-boy-img-1x.png') +
+            ' 1x,' +
+            require('../../../images/desk-boy-img-2x.png') +
+            ' 2x'
+          }
+          media="(min-width: 1200px)"
+        />
+        <img
+          src={require('../../../images/mob-boy-img-1x.png')}
+          alt="user"
+          height="124"
+        />
+      </picture>
+
       <div className={css.startPageTitileBox}>
         <p>Logo icon svg</p>
         <h1 className={css.startPageTitile}>Task Pro</h1>
