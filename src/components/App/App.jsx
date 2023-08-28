eae327762bbeff00d2050c79b5db125d05084532
Oleksandr <115025828+Oleksandr-Tsimbaliuk@users.noreply.github.com>
2023-08-28 12:49:50 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,17 +3,18 @@
 // import ScreensPage from 'pages/ScreenPage/ScreenPage';
 import { LoginForm, RegisterForm, StartPageView } from 'components';
 // import PrivateRoute from 'components/Routes/PrivatRoute/PrivateRoute';
-import HomePage from '../../pages/HomePage/HomePage';
+// import HomePage from '../../pages/HomePage/HomePage';
 import AuthPage from 'pages/AuthPage';
 
-import Page from 'pages/SideBarTopBar';
 import { Route, Routes } from 'react-router-dom';
 import { StartPage } from 'pages';
+import HomePage from 'pages/HomePage/HomePage';
+
 export default function App() {
   return (
     <>
       <Routes>
-        {/* Auth Routes  */}
+        {/* Auth Routes */}
         <Route path="/" element={<StartPage />}>
           <Route path="/home" element={<HomePage />} />
           <Route index element={<StartPageView />} />
@@ -22,12 +23,9 @@ export default function App() {
             <Route path="login" element={<LoginForm />} />
           </Route>
         </Route>
-
-        <Route path="/SideBarTopBar" element={<Page />} />
         {/* <Route path="/" element={<Login />} />
-        <Route path="/kak" element={<ScreensPage />} />
         
-        <Route path="/kaka" element={<ScreensPage />} />
+        <Route path=":boardId" element={<ScreensPage />} /> />
         <Route path="/home" element={<HomePage />} />
          <Route
         {/* Auth Routes 
