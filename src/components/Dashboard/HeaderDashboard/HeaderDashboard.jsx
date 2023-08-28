83eb52d76de8900c2e24f559afbffcf7ae981f76
PilyaP <ppaexpert@gmail.com>
2023-08-28 14:32:20 +0300

    підправив...очикую Пашу ввечері чат бот дал коментарі...щоб легче розібратись:)

--- src/components/Dashboard/HeaderDashboard/HeaderDashboard.jsx
+++ src/components/Dashboard/HeaderDashboard/HeaderDashboard.jsx
@@ -4,11 +4,14 @@ import css from './HeaderDashboard.module.css';
 const HeaderDashboard = ({ title }) => {
   return (
     <div>
+      {/* Відображаємо заголовок дошки */}
       <h1 className={css.title}>{title}</h1>
       <div>
+        {/* Відображаємо компонент Filter */}
         <Filter />
       </div>
     </div>
   );
 };
+
 export default HeaderDashboard;
--- src/pages/ScreenPage/ScreenPage.jsx
+++ src/pages/ScreenPage/ScreenPage.jsx
@@ -10,15 +10,25 @@ import { MainDashboard } from 'components/Dashboard/MainDashboard/MainDashboard'
 
 const ScreenPage = () => {
   const navigate = useNavigate();
+
+  // Отримуємо параметр boardId з URL
   const { boardId } = useParams();
+
+  // Використовуємо useDispatch для виклику Redux-дій
   const dispatch = useDispatch();
+
+  // Використовуємо useSelector для отримання списку дошок зі стану Redux
   const boards = useSelector(selectBoards);
+
+  // Знаходимо дошку за допомогою boardId
   const board = boards.find(item => item._id === boardId);
 
+  // Викликаємо дію getBoardByID(boardId) під час монтажу компонента
   useEffect(() => {
     dispatch(getBoardByID(boardId));
   }, [boardId, dispatch]);
 
+  // Перенаправляємо на /home, якщо дошка не знайдена
   useEffect(() => {
     if (!board) {
       navigate('/home');
@@ -27,9 +37,12 @@ const ScreenPage = () => {
 
   return (
     <>
+      {/* Відображаємо заголовок та основний вміст сторінки, якщо дошка знайдена */}
       {board && (
         <>
+          {/* Передаємо заголовок дошки в HeaderDashboard */}
           <HeaderDashboard title={board.title} />
+          {/* Передаємо айді дошки в MainDashboard */}
           <MainDashboard id={board._id} />
         </>
       )}
