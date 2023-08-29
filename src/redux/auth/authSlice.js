2e753d0c46539cd46af987eb96088e846685f891
SyvonenkoOleh <115041334+SyvonenkoOleh@users.noreply.github.com>
2023-08-29 04:46:01 +0300

    1.7

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -16,6 +16,7 @@ const authInitialState = {
     avatarURL: '',
     theme: 'dark',
     password: '',
+    displays: '',
   },
   token: '',
   isLoggedIn: false,
--- src/redux/boards/selectors.js
+++ src/redux/boards/selectors.js
@@ -1,6 +1,10 @@
-export const selectBoards = state => state.boards;
+export const selectBoards = state => state.boards.boards;
 
 export const selectCurrentBoard = state => state.boards.currentBoard;
 
 export const selectCurrentBoardData = state =>
   state.boards[state.boards.currentBoard];
+
+export const selectIsLoading = state => state.boards.isLoading;
+
+export const selectError = state => state.boards.error;
