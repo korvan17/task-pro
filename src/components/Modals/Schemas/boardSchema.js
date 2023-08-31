3675d04f97a89e7aa5779ce623315b049133818e
bakmaiev <thisyoungrhyme@gmail.com>
2023-08-31 20:00:18 +0300

    Add validation to AddEditBoard

--- src/components/Modals/Schemas/boardSchema.js
+++ src/components/Modals/Schemas/boardSchema.js
@@ -1,7 +1,9 @@
 import * as yup from 'yup';
 
 const boardSchema = yup.object({
-  title: yup.string().trim().required(),
+  title: yup.string().trim().required('Title is required'),
+  //   background: yup.string().required('Please select a background'),
+  //   icon: yup.string().required('Please select an icon'),
 });
 
 export default boardSchema;
