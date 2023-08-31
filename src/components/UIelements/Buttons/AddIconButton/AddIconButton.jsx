c6583da3dac4f0abbe19de037da2ae2df5f834ee
Yurii Soroka <yurisoroka165@gmail.com>
2023-08-31 07:25:23 +0300

    themeUp

--- src/components/UIelements/Buttons/AddIconButton/AddIconButton.jsx
+++ src/components/UIelements/Buttons/AddIconButton/AddIconButton.jsx
@@ -1,18 +1,24 @@
 import Buttons from '../Button';
 import css from './AddIconButton.module.css';
 import sprite from '../../../../icons/sprite.svg';
+import { useTheme } from '@emotion/react';
 
 export default function AddIconButton({
   buttonType = 'button',
   children,
   className,
 }) {
+  const theme = useTheme();
   return (
     <Buttons
       className={` ${className} ${children ? css.button : css.buttonNoTxt}`}
       buttonType={buttonType}
     >
       <svg
+        style={{
+          backgroundColor: theme.popUp.buttonIconBackground,
+          stroke: theme.popUp.buttonIconPlusFill,
+        }}
         className={`${css.icon} ${!children && css.iconNoTxt}`}
         width={28}
         height={28}
