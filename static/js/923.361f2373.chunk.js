cf5bd61ffa2e3376aec1ca5db6e41f723af74c8d
korvan17 <114874202+korvan17@users.noreply.github.com>
2023-08-31 19:32:30 +0000

    Deploying to gh-pages from @ korvan17/task-pro@793df826a79e79f111efbd2cec1e215a2a0175a2 🚀

--- static/js/923.361f2373.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_task_pro=self.webpackChunkgoit_task_pro||[]).push([[923],{923:function(t,r,s){s.r(r);s(2791);var e=s(1166),n=s(184);r.default=function(){return(0,n.jsx)(e.N7,{children:(0,n.jsx)(e.rU,{})})}}}]);
-//# sourceMappingURL=923.361f2373.chunk.js.map
