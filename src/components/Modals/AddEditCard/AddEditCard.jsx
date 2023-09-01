955661d6e2c9bdf5ea0a546244532a9b3feb0de2
bakmaiev <thisyoungrhyme@gmail.com>
2023-09-01 14:27:40 +0300

    Add validation in AddEditCard using Formik

--- src/components/Modals/AddEditCard/AddEditCard.jsx
+++ src/components/Modals/AddEditCard/AddEditCard.jsx
@@ -1,39 +1,49 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import css from './AddEditCard.module.css';
 import iconDefs from '../../../icons/sprite.svg';
 import ColorPicker from 'components/UIelements/ColorPicker/ColorPicker';
 import { AddIconButton, Calendar } from 'components';
 import { useState } from 'react';
 import { useTheme } from '@emotion/react';
+import { useDispatch } from 'react-redux';
+import { addCard, editCard } from 'redux/сard/сardOperations';
+import cardSchema from '../Schemas/cardSchema';
 
-export default function AddEditCard({
-  title,
-  description,
-  isEditing,
-  onSubmit,
-  onClose,
-}) {
-  const [inputValue, setInputValue] = useState('');
-  const [textareaValue, setTextareaValue] = useState('');
+export default function AddEditCard({ isEditing, onClose, cardId }) {
+  const theme = useTheme();
+  const dispatch = useDispatch();
   const [color, setColor] = useState('');
-
-  const handleInputChange = e => {
-    setInputValue(e.target.value);
-  };
-
-  const handleTextareaChange = e => {
-    setTextareaValue(e.target.value);
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSubmit(inputValue, textareaValue, color);
-    onClose();
-  };
 
   const handleSelectedColorChange = selectedColor => {
     setColor(selectedColor);
   };
-  const theme = useTheme();
+
+  const handleSubmit = async (values, { resetForm }) => {
+    try {
+      if (!isEditing) {
+        await dispatch(
+          addCard({
+            title: values.title,
+            description: values.description,
+            color,
+          })
+        );
+      } else {
+        await dispatch(
+          editCard({
+            title: values.title,
+            description: values.description,
+            color,
+            cardId,
+          })
+        );
+      }
+      resetForm();
+      onClose();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
@@ -49,54 +59,73 @@ export default function AddEditCard({
       <h3 style={{ color: theme.popUp.titleColor }} className={css.titleCard}>
         {!isEditing ? 'Add card' : 'Edit card'}
       </h3>
-      <form onSubmit={handleSubmit}>
-        <label className={css.label}>
-          <input
-            style={{
-              color: theme.popUp.inputTextColor,
-              borderColor: theme.popUp.inputBorderColor,
-              '::placeholder': { color: theme.popUp.inputPlaceholderColor },
-            }}
-            className={css.input}
-            type="text"
-            name="title"
-            placeholder="Title"
-            onChange={handleInputChange}
-          />
-        </label>
-        <label className={css.label}>
-          <textarea
-            style={{
-              color: theme.popUp.inputTextColor,
-              borderColor: theme.popUp.inputBorderColor,
-              '::placeholder': { color: theme.popUp.inputPlaceholderColor },
-            }}
-            className={css.textarea}
-            type="text"
-            name="description"
-            placeholder="Description"
-            onChange={handleTextareaChange}
-          />
-        </label>
-        <ColorPicker onSelectedColorChange={handleSelectedColorChange} />
-        <div>
-          <span
-            style={{ color: theme.popUp.iconsTextColor }}
-            className={css.calendarTitle}
-          >
-            Deadline
-          </span>
-          <Calendar />
-        </div>
-        <AddIconButton className={css.btn}>
-          <span
-            style={{ color: theme.popUp.buttonTextColor }}
-            className={css.btnSumbitAction}
-          >
-            {!isEditing ? 'Add' : 'Edit'}
-          </span>
-        </AddIconButton>
-      </form>
+      <Formik
+        initialValues={{
+          title: '',
+          description: '',
+          color: '',
+        }}
+        onSubmit={handleSubmit}
+        validationSchema={cardSchema}
+      >
+        <Form>
+          <label className={css.label}>
+            <ErrorMessage
+              className={css.errorMessage}
+              name="title"
+              component="p"
+            />
+            <Field
+              style={{
+                color: theme.popUp.inputTextColor,
+                borderColor: theme.popUp.inputBorderColor,
+                '::placeholder': { color: theme.popUp.inputPlaceholderColor },
+              }}
+              className={css.input}
+              type="text"
+              name="title"
+              placeholder="Title"
+            />
+          </label>
+          <label className={css.label}>
+            <ErrorMessage
+              className={css.errorMessage}
+              name="description"
+              component="p"
+            />
+            <Field
+              as="textarea"
+              style={{
+                color: theme.popUp.inputTextColor,
+                borderColor: theme.popUp.inputBorderColor,
+                '::placeholder': { color: theme.popUp.inputPlaceholderColor },
+              }}
+              className={css.textarea}
+              type="text"
+              name="description"
+              placeholder="Description"
+            />
+          </label>
+          <ColorPicker onSelectedColorChange={handleSelectedColorChange} />
+          <div>
+            <span
+              style={{ color: theme.popUp.iconsTextColor }}
+              className={css.calendarTitle}
+            >
+              Deadline
+            </span>
+            <Calendar />
+          </div>
+          <AddIconButton buttonType="submit" className={css.btn}>
+            <span
+              style={{ color: theme.popUp.buttonTextColor }}
+              className={css.btnSumbitAction}
+            >
+              {!isEditing ? 'Add' : 'Edit'}
+            </span>
+          </AddIconButton>
+        </Form>
+      </Formik>
     </>
   );
 }
