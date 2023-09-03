2f3dc4d0b37a080009bbb2f2ea189d4654daf1f2
PilyaP <ppaexpert@gmail.com>
2023-09-03 22:46:54 +0300

    push unshift

--- src/redux/boards/slice.js
+++ src/redux/boards/slice.js
@@ -58,7 +58,7 @@ const boardsSlice = createSlice({
       .addCase(addBoard.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
-        state.boards.push({ ...action.payload });
+        state.boards.unshift({ ...action.payload });
       })
 
       .addCase(deleteBoard.fulfilled, (state, action) => {
