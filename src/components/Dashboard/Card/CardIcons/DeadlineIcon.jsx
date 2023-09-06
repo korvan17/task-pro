2c88e6a0805afecdc3a7e9c02d0d32b240b02d73
Yurii Soroka <yurisoroka165@gmail.com>
2023-09-06 15:15:14 +0300

    up

--- src/components/Dashboard/Card/CardIcons/DeadlineIcon.jsx
+++ src/components/Dashboard/Card/CardIcons/DeadlineIcon.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import sprite from '../../../../icons/sprite.svg'
-import css from './CardIcons.module.css'
+import sprite from '../../../../icons/sprite.svg';
+import css from './CardIcons.module.css';
+import { useTheme } from '@emotion/react';
 
 export default function DeadlineIcon() {
+  const theme = useTheme();
   return (
-    <svg className={`${css.icon} ${css.iconBell}`} width="16" height="16">
+    <svg
+      style={{ stroke: theme.card.bellIconFill }}
+      className={`${css.icon} ${css.iconBell}`}
+      width="16"
+      height="16"
+    >
       <use xlinkHref={`${sprite}#icon-bell`} />
     </svg>
   );
